2087535234761ce9e74876a06add80e5cf5eecb3
DanMckenna <dmckenna728@gmail.com>
2024-03-23 17:06:53 -0400

    Updated the NodejsConsoleDemoApp project with additional string manipulation techniques and examples showing functions and conditionals.

--- Demonstration Projects/JavaScript Projects/NodejsConsoleDemoApp/NodejsConsoleDemoApp.js
+++ Demonstration Projects/JavaScript Projects/NodejsConsoleDemoApp/NodejsConsoleDemoApp.js
@@ -1,34 +1,62 @@
 /* Created by Daniel Mckenna
- * This is a simple console application written in JavaScript for use in Node.js
- * This application will write a series of messages to the console showcasing fundamental techniques including:
+ * A simple console application written in JavaScript for use in Node.js
+ * Writes a series of messages to the console showcasing fundamental techniques including:
  *  string manipulation
- *  user input
  *  functions
- *  arrays
  *  conditionals
+ *  loops
  */
 
+// enable strict mode to prevent any accidental undeclared variables/objects from being used
 'use strict';
 
-// write a simple message to the console
-console.log("We will now begin to demonstrate string manipulation techniques in JavaScript via the console");
+// a function showcasing string manipulation techniques
+function testStringManipulation() {
+    console.log("**********STRING TECHNIQUES**********");
+    console.log("We will now begin to demonstrate string manipulation techniques in JavaScript via the console");
 
-// write a message using string concatenation
-console.log("This message was made using " + "string concatenation");
+    // write a message using string concatenation
+    console.log("This message was made using " + "string concatenation");
 
-// write a message using string concatenation from a variable
-let concatenationStr = "string concatenation";
-console.log("This message was made using " + concatenationStr + " from a variable");
+    // write a message using string concatenation from a variable
+    let concatenationStr = "string concatenation";
+    console.log("This message was made using " + concatenationStr + " from a variable");
 
-// write a message to the console using string interpolation
-// note that string interpolation uses backticks (`) as opposed to single quotes (') or double quotes (")
-let interpolationStr1 = "string";
-let interpolationStr2 = "interpolation";
-console.log(`This message was made using ${interpolationStr1} ${interpolationStr2}`);
+    // write a message to the console using string interpolation
+    // note that string interpolation uses backticks (`) as opposed to single quotes (') or double quotes (")
+    let interpolationStr1 = "string";
+    let interpolationStr2 = "interpolation";
+    console.log(`This message was made using ${interpolationStr1} ${interpolationStr2}`);
 
-// write a multi-line message to the console using the \n newline character
-console.log("This is a multi-line message\nthat was made using\nthe \\n newline character");
+    // write a message to the console that includes double quotes via the escape character
+    console.log("This message uses \\ as an escape character in order to print \"double quotes\" to the console");
 
-// write a multi-line message to the console using backticks (`)
-console.log(`This is a multi-line message
-that was made using backticks`);
+    // write a multi-line message to the console using the \n newline character
+    console.log("\nThis is a multi-line message\nthat was made using\nthe \\n newline character\n");
+
+    // write a multi-line message to the console using backticks (`)
+    console.log(`This is a multi-line message
+                 that was made using backticks
+                 `);
+
+    // display the total number of letters in the word "Cranberries"
+    let testString1 = "Cranberries";
+    console.log("The total number of letters in the word \"" + testString1 + "\" is " + (testString1.length).toString());
+
+    // loop through each letter in the test string and display their position number and value
+    for (let i = 0; i < testString1.length; i++) {
+        console.log("The letter in position " + (i + 1).toString() + " of the word \"Cranberries\" is " + testString1[i]);
+    }
+
+    // check if a given string appears within another string
+    // if so then return the position of the first instance of the substring
+    if (testString1.includes("berries")) {
+        console.log("The word \"berries\" appears in the word \"Cranberries\" at position " + ((testString1.indexOf("berries") + 1)).toString());
+    }
+    else {
+        console.log("The word \"berries\" does NOT appear in the test string");
+    }
+}
+
+// execute the testStringManipulation() function
+testStringManipulation();
